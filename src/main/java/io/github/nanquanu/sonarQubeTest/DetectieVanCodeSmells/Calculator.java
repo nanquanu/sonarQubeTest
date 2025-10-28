ec50c4a5b2b43f6e0cbc1fd01ba9f47c9a03e0b8
DrOteonu-on-Shaduanu <shaduana@camping.de.bolle.neus.org>
2025-10-28 17:27:09 +0100

    minor

--- src/main/java/io/github/nanquanu/sonarQubeTest/DetectieVanCodeSmells/Calculator.java
+++ src/main/java/io/github/nanquanu/sonarQubeTest/DetectieVanCodeSmells/Calculator.java
@@ -3,7 +3,7 @@ package io.github.nanquanu.sonarQubeTest;
 public class Calculator {
    public int add(int a, int b) { return a + b; }
    public int subtract(int a, int b) { return a - b; }
-   public int multiply(int a, int b) { return a * b; }
+   public int multiply(int a, int b) { return a * b ; }
 
    public int divide(int a, int b) {
       if(b == 0) return 0; // Geen foutafhandeling
