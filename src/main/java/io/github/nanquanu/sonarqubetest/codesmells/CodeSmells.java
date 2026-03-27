e5caf6ee2447a75eb3cdaf25c538fc9a711eb950
DrOteonu <oteonu@nanquanu.org>
2026-03-27 20:09:07 +0100

    self explanatory

---
+++ src/main/java/io/github/nanquanu/sonarqubetest/codesmells/CodeSmells.java
@@ -0,0 +1,44 @@
+package io.github.nanquanu.sonarqubetest.codesmells;
+
+import java.util.*;  
+import javax.annotation.Nonnull;  
+
+public class BadCode implements Cloneable {  
+    
+    private static int counter = 0;  
+    
+    public static void main(String[] args) {
+        BadCode obj = new BadCode();
+        obj.incrementCounter();  
+        
+        switch (args.length) {  
+            case 0:
+                System.out.println("No args");
+                break;
+            case 1:
+                System.out.println("One arg");  
+                break;
+        }
+        
+        if (true)  
+            System.out.println("No braces!");
+        
+        for (int i = 0; i < 5; i = i + 1) {  
+            if (i == 2) 
+            else if (i == 3)  
+                System.out.println("Bad if chain");
+        }
+        
+        String s = "hello\u0000world";  
+        System.out.println(s.replaceAll("l", "x"));  
+    }
+    
+    public void incrementCounter() {
+        counter++;  
+    }
+    
+    @Override
+    public boolean equals(@Nonnull Object obj) {  
+        return this == obj;
+    }
+}
